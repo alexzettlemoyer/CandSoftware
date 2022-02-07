b1d0a66ad24fd95bd2aa87d7fd5f42a92fcae9ef
alzettle <alzettle@ncsu.edu>
2022-02-07 16:25:11 -0500

    base 10 functionality

--- p2/calc.c
+++ p2/calc.c
@@ -7,49 +7,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+	parseTerm
+	calculates the result of one * or / operation
+	if the 'term' only has one number, it is returned
+	@return long result of the operation
+*/
 static long parseTerm()
 {
 	long value;
-	long val1 = 0;
+	long val1 = parseValue();
 	long val2 = 1;
 	
-	val1 = parseValue();
-	char ch = skipSpace();
+	char operator = ' ';
 	
-	if (ch == '+' || ch == '-' || ch == '\n' || ch == EOF)
-		ungetc(ch, stdin);
-		return val1;
+	while (operator != '+' && operator != '-' && operator != '\n' && operator != EOF) {
 		
-	val2 = parseValue();
+		operator = skipSpace();
+		
+		if (operator == '+' || operator == '-' || operator == '\n' || operator == EOF) {
+			ungetc(operator, stdin);
+			return val1;
+		}
+		
+		val2 = parseValue();
 	
-	if (ch == '*')
-		value = times(val1, val2);
-	if (ch == '/')
-		value = divide(val1, val2);
+		if (operator == '*')
+			value = times(val1, val2);
+		if (operator == '/')
+			value = divide(val1, val2);
+		val1 = value;
+	}
 	
 	return value;
 }
 
+/**
+	starting point of program
+	loops over + and - operations and calculates them
+	delegating to parseTerm
+	until reaching EOF or a newLine
+	@return exit status
+*/
 int main() 
 {
-	long value;
-	long val1;
-	long val2;
+	long val2 = 0;
 	char operator = ' ';
+	long value = parseTerm();
 	
 	while (operator != EOF && operator != '\n') {
-		val1 = parseTerm();
+
 		operator = skipSpace();
 		
-		if (operator == EOF || operator == '\n')
+		if (operator == EOF || operator == '\n') {
 			break;
-		
+		}
+
 		val2 = parseTerm();
 		
-		if (operator == '+') 
-			value += plus(val1, val2);
-		if (operator == '-')
-			value += minus(val1, val2);
+		if (operator == '+') {
+			value = plus(value, val2);
+		}
+		else if (operator == '-') {
+			value = minus(value, val2);
+		}
 	}
 	printValue(value);
 	return 0;
