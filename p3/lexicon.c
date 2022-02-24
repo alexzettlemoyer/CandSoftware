e857b5ee1128b36d377a124390af4bb8a354dadf
alzettle <alzettle@ncsu.edu>
2022-02-24 13:09:10 -0500

    initial push

---
+++ p3/lexicon.c
@@ -0,0 +1,9 @@
+#include "lexicon.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "io.h"
+
+/** Large prime multiplier used to choose a word pseudo-randomly. */
+#define MULTIPLIER 4611686018453
---
+++ p3/lexicon.h
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+/** Maximum lengh of a word on the word list. */
+#define WORD_LEN 5
+
+/** Maximum number of words on the word list. */
+#define WORD_LIMIT 100000
