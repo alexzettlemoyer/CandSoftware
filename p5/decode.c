80d1ced4381fad16aba63ae158c0bca5446344ef
alzettle <alzettle@ncsu.edu>
2022-04-07 22:48:47 -0400

    formatting

--- p5/decode.c
+++ p5/decode.c
@@ -1,6 +1,10 @@
 /**
 	@file decode.c
 	@author Alex Zettlemoyer
+	decode program
+	usage: decode <input-file> <output-file>
+	reads in the input.bin file and decodes the characters
+	prints the decoded characters to the output.txt file
 */
 #include "state24.h"
 #include "filebuffer.h"
@@ -11,9 +15,21 @@
 #include <string.h>
 #include <stdbool.h>
 
-
+/** expected number of command line arguments */
 #define ARG_NUM 3
+/** filled state bit length */
+#define BITLENGTH 24
+/** number of bytes in the buffer array */
+#define NUM_BYTES 3
 
+/**
+	invalidFile
+	prints the invalid file message to standard error
+	closes the given file and frees the given filebuffer
+	exits the program unsuccessfully
+	@param file to close
+	@param filebuffer to free
+*/
 void invalidFile( FILE *file, FileBuffer *fb )
 {
 	fprintf( stderr, "Invalid input file\n");
@@ -22,6 +38,13 @@ void invalidFile( FILE *file, FileBuffer *fb )
 	exit( EXIT_FAILURE );
 }
 
+/**
+	starts the program
+	reads in the input.bin file and decodes the characters
+	prints the decoded characters to the output.txt file
+    @param args the number of command line arguments
+    @param argv the argument vector of character array arguments
+*/
 int main( int args, char *argv[] ) 
 {
 	
@@ -33,56 +56,67 @@ int main( int args, char *argv[] )
 	char *inputfile = argv[ 1 ];
 	char *outputfile = argv[ 2 ];
 	
-	
+	// open the input file
 	FILE *input = fopen( inputfile, "r" );
 	if ( !input ) {
 		perror( inputfile );
 		exit( EXIT_FAILURE );
 	}
 	
+	// initialize the filebuffer
 	FileBuffer *filebuffer = makeFileBuffer();
 	
+	// initialize the state
 	State24 state;
 	initState( &state );
 	
-	
 	char ch = '\0';
-	//char nextCh = '\0';
 	bool equals = false;
 	
+	// while there's more input
 	while ( fscanf( input, "%c", &ch ) != EOF ) {
 			
+		// if the character is a valid encoding character
 		if ( validChar(ch) )
 			addChar( &state, ch );
+		// if the character is an equals
 		else if ( ch == '=' )
 			equals = true; 
+		// if the character isn't valid, isn't =, and isn't space
 		else if ( !isspace( ch ) )
 			invalidFile( input, filebuffer );
 		
+		// if the character is after an = and isn't an =
 		if ( equals && ch != '=' && !isspace( ch ) )
 			invalidFile( input, filebuffer );
-								
-		if ( state.bitlength == 24 ) {
-			byte bytes[ 3 ];
-			memset( bytes, '\0', 3 * sizeof( byte ));
+		
+		// if the state is full						
+		if ( state.bitlength == BITLENGTH ) {
+			byte bytes[ NUM_BYTES ];
+			memset( bytes, '\0', NUM_BYTES * sizeof( byte ));
 			
+			// get the bytes
 			int length = getBytes( &state, bytes );
 			
+			// add the bytes to the file buffer
 			for ( int m = 0; m < length; m++ )
 				appendFileBuffer( filebuffer, bytes[ m ]);
 		}
 	}
 	
+	// if there's leftover bytes in the state
 	if ( state.bitlength > 0 ) {
-		byte bytes[ 3 ];
-		memset( bytes, '\0', 3 * sizeof( byte ));
+		byte bytes[ NUM_BYTES ];
+		memset( bytes, '\0', NUM_BYTES * sizeof( byte ));
 			
 		int length = getBytes( &state, bytes );
-			
+		
+		// add the bytes to the file buffer 
 		for ( int m = 0; m < length; m++ )
 			appendFileBuffer( filebuffer, bytes[ m ]);
 	}
 	
+	// save the file buffer to the output file
 	saveFileBuffer( filebuffer, outputfile );
 	
 	freeFileBuffer( filebuffer );
--- p5/encode.c
+++ p5/encode.c
@@ -1,6 +1,13 @@
 /** 
-	@file encode.c
-	@author Alex Zettlemoyer
+    @file encode.c
+    @author Alex Zettlemoyer
+    encode program
+    usage: encode [-b] [-p] <input-file> <output-file>
+    reads in the input.txt file and encodes the characters
+    prints the encoded bytes to the output.bin file
+    optional command line arguments -b -p
+    -b disables line breaks (normally after 76 characters)
+    -p disables padding (extra = character for empty bytes)
 */
 
 #include "state24.h"
@@ -11,81 +18,101 @@
 #include <stdbool.h>
 #include <string.h>
 
+/** max number of characters on a line to output */
 #define LINE_LIMIT 76
+/** number of characters in the buffer */
 #define NUM_CHARS 4
+/** number of bytes in the state */
 #define NUM_BYTES 3
 
-void usage()
-{
-	fprintf( stderr, "usage: encode [-b] [-p] <input-file> <output-file>\n");
-	exit( EXIT_FAILURE );
-}
-
+/**
+    starts the program
+    reads in the inputfile ( second to last command line argument )
+    writes the encoded binary text to the output file ( last argument )
+    optional -p option to disable padding
+    optional -b option to disable line breaks
+    @param args the number of command line arguments
+    @param argv the argument vector of character array arguments
+*/
 int main( int args, char *argv[] )
 {
-	bool padding = true;
-	bool lineBreaks = true;
-	
-	// not enough command line arguments
-	if ( args < 3 ) 
-		usage();
-	// if option is added, check that first two arguments are -b or -p
-	if ( args > 3 && strcmp( argv[1], "-b") != 0 && strcmp( argv[1], "-p") != 0)
-			usage();
-	if ( args > 4 && strcmp( argv[2], "-b") != 0 && strcmp( argv[2], "-p") != 0)
-			usage();
-	
-	// check for line break and padding disabled option
-	if ( strcmp( argv[1], "-b") == 0 || strcmp( argv[2], "-b") == 0)
-		lineBreaks = false;
-	if ( strcmp( argv[1], "-p") == 0 || strcmp( argv[2], "-p") == 0 )
-		padding = false;
-		
-	char *inputfile = argv[ args - 2 ];
-	char *outputfile = argv[ args - 1 ];
-	
-	FileBuffer *filebuffer = loadFileBuffer( inputfile );
-	
-	FILE *output = fopen( outputfile, "w");
-	if ( !output ) {
-		perror( outputfile );
-		exit( EXIT_FAILURE );
-	}
-	
-	State24 state;
-	initState( &state );
-	
-	int counter = 0;
+    bool padding = true;
+    bool lineBreaks = true;
+    
+    // not enough command line arguments
+    if ( args < 3 ) {
+        fprintf( stderr, "usage: encode [-b] [-p] <input-file> <output-file>\n");
+        exit( EXIT_FAILURE );
+    }
+    
+    // if option is added, check that first two arguments are -b or -p
+    for ( int i = 1; i < args - 2; i++ ) {
 
-	for ( int i = 0; i < (*filebuffer).length; i++ ) {
-		addByte( &state, (*filebuffer).data[i] );
-		
-		
-		if ( state.length == NUM_BYTES || i == (*filebuffer).length - 1 ) {
-			char buffer[ NUM_CHARS ];
-			int match = getChars( &state, buffer );
-			
-			if ( padding ) {
-				for ( int i = match; i < NUM_CHARS; i++ ) {
-					buffer[i] = '=';
-				}
-				match = NUM_CHARS;
-			} 
-			
-			for ( int m = 0; m < match; m++ ) {
-				fprintf( output, "%c", buffer[m] );
-				counter++;
-				
-				if ( counter == LINE_LIMIT && lineBreaks ) {
-					fprintf( output, "\n");
-					counter = 0;
-				}
-			}
-		}
-	}
-	
-	fprintf( output, "\n" );
-	fclose( output );
-	freeFileBuffer( filebuffer );
-	exit( EXIT_SUCCESS );
+        // check for line break and padding disabled option
+        if ( strcmp( argv[ i ], "-b") == 0 )
+            lineBreaks = false;
+        else if ( strcmp( argv[ i ], "-p") == 0 )
+            padding = false;
+        else {
+            fprintf( stderr, "usage: encode [-b] [-p] <input-file> <output-file>\n");
+            exit( EXIT_FAILURE );
+        }
+    }
+        
+    char *inputfile = argv[ args - 2 ];
+    char *outputfile = argv[ args - 1 ];
+    
+    // load the inputfile into the file buffer
+    FileBuffer *filebuffer = loadFileBuffer( inputfile );
+    
+    // open the outputfile
+    FILE *output = fopen( outputfile, "w");
+    if ( !output ) {
+        perror( outputfile );
+        exit( EXIT_FAILURE );
+    }
+    
+    // initialize the state
+    State24 state;
+    initState( &state );
+    
+    int counter = 0;
+
+    // for each byte in the filebuffer
+    for ( int i = 0; i < (*filebuffer).length; i++ ) {
+    
+        // add the byte
+        addByte( &state, (*filebuffer).data[i] );
+        
+        // if the state is full or we're at the last byte in the filebuffer
+        if ( state.length == NUM_BYTES || i == (*filebuffer).length - 1 ) {
+            char buffer[ NUM_CHARS ];
+            int match = getChars( &state, buffer );
+            
+            // if padding is enabled
+            if ( padding ) {
+                // fill the empty characters with =
+                for ( int i = match; i < NUM_CHARS; i++ ) {
+                    buffer[i] = '=';
+                }
+                match = NUM_CHARS;
+            } 
+            // for each matched character
+            for ( int m = 0; m < match; m++ ) {
+                fprintf( output, "%c", buffer[m] );
+                counter++;
+                
+                // if linebreaks are enabled and we're at the limit
+                if ( counter == LINE_LIMIT && lineBreaks ) {
+                    fprintf( output, "\n");
+                    counter = 0;
+                }
+            }
+        }
+    }
+    
+    fprintf( output, "\n" );
+    fclose( output );
+    freeFileBuffer( filebuffer );
+    exit( EXIT_SUCCESS );
 }
